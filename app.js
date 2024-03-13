710ecc63bf6beda618629d35815536107f5ccf25
Yaroslav Kuzenko <yaroslavkuzenko02@gmail.com>
2024-03-13 20:35:04 +0200

    2 step

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from "express"
+import parcels from "./data/parcels.json"
+import _ from "lodash"
 
 const PORT = 3000
 const server = express()
+const BASE_PARCELS_URL = '/api/v1/parcels'
 
 console.log("Pes Patronchuk")
 
@@ -15,4 +18,19 @@ server.listen(PORT, () =>{
 
 server.get('/', (req, res) => {
     res.send("Pryvit")
+})
+
+server.get(BASE_PARCELS_URL, (req, res) => {
+    res.json(parcels)
+})
+
+server.get(BASE_PARCELS_URL + "/:id", (req, res) => {
+    const id = req.params.id
+    const parcel = _.find(parcels, parcel => parcel.id === id)
+
+    if (parcel){
+        res.json(parcel)
+    }else {
+        res.send("Сталаси біда. Ніц нема")
+    }
 })
